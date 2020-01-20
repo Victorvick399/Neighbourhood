8fc132e7a034518b00ce81bf55e2457ef0849fca
Victorvick399 <victorvick399@gmail.com>
2020-01-20 09:21:14 +0300

    Create forms for the app

---
+++ app/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import *
+
+# class CommentForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Comment
+#         exclude = ['poster', 'imagecommented']
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class AddHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ['user_profile', 'profile']
+
+
+class AddBizForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['biz_owner', 'biz_hood']
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['poster', 'post_hood']
+
